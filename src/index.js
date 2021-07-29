2377f82c2e75a31d26c6769f99e0c83f823758a4
Dino <wsxqaz12322@gmail.com>
2021-07-30 01:24:11 +0800

    feat : layout dev

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import "./index.css";
+
+const btn = document.getElementById('bth')
+const content = document.getElementById('text')
+const box = document.getElementById('box')
+
+btn.addEventListener('click', function () {
+  content.classList.remove('text-fb')
+
+  box.innerHTML = '<p class="text-fb">test</p>'
+})
